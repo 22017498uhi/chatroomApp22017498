3c8c79e33f4d3d2c0cf25306977fc2df8cc548e7
22017498uhi <113307467+22017498uhi@users.noreply.github.com>
2023-04-09 20:13:09 +0100

    gmail login sso redirect complete

--- chatroom/src/App.js
+++ chatroom/src/App.js
@@ -1,11 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Chatroom from './components/Chatroom';
+
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+  Outlet
+} from "react-router-dom";
+
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { useEffect, useState } from 'react';
+
+function PrivateRoute({authenticated}) {
+  return (
+    authenticated === true ? (
+      <Outlet />
+    )
+   : (
+    <Navigate
+      to={{pathname:"/"}}
+      />
+  )
+  );
+}
+
+function PublicRoute({authenticated}) {
+  return (
+    authenticated === false ? (
+      <Outlet />
+    ) : (
+      <Navigate to="/chatroom" />
+    )
+  );
+}
+
+
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect( () => {
+    console.log('authenticated', authenticated);
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if(user){
+        setAuthenticated(true)
+      }else {
+        setAuthenticated(false)
+      }
+    })
+  }, []);
+
   return (
     <div>
-      <Login />
+      <Router>
+        <Routes>
+
+          <Route exact path='/' element={<PublicRoute authenticated={authenticated} />} >
+            <Route exact path='/' element={<Login />}></Route>
+          </Route>
+
+          <Route path='/chatroom' element={<PrivateRoute authenticated={authenticated} />} >
+            <Route exact path='/chatroom' element={<Chatroom />}></Route>
+          </Route>
+
+        </Routes>
+        </Router>
     </div>
   );
 }
